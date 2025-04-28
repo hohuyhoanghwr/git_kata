3ea3e60d5a91eae6322da0ed9f64909b096ab661
HoangHo <s_ho24@stud.hwr-berlin.de>
2025-04-28 16:08:11 +0200

    new function

--- utils.py
+++ utils.py
@@ -4,3 +4,14 @@ def load_data(path):
     df = pd.read_csv(path)
     df = df[df['sex'] == "male"]
     return df
+
+def clean_data(df):
+    """
+    comment1
+    comment2
+    comment3
+    """
+    df = df.dropna(axis=1)
+    for col in df.select_dtypes(include='object').columns:
+        df[col] = df[col].str.lower()
+    return df
